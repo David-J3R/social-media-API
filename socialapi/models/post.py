83d03c52a2ac9d68bd48482a45013dd87e5c7c88
David-J3R <181415601+David-J3R@users.noreply.github.com>
2025-09-28 13:00:54 +0200

    First Social API app, with a structure code

--- socialapi/models/post.py
+++ socialapi/models/post.py
@@ -7,3 +7,27 @@ class UserPostIn(BaseModel):
 
 class UserPost(UserPostIn):  # Class inheritance
     id: int
+
+
+# ----- Comments -----
+class CommentIn(BaseModel):
+    body: str
+    post_id: int
+
+
+class Comment(CommentIn):
+    id: int
+
+
+class UserPostWithComments(BaseModel):
+    post: UserPost
+    comments: list[Comment]
+
+
+# Example of object
+"""
+{
+"post": {"id": 1, "body": "My first post"},
+"comments": [{"id": 1, "post_id": 1, "body": "Great post!"}, {"id": 2, "post_id": 2, "body": "Thanks for sharing!"}]
+}
+"""
---
+++ socialapi/routers/post.py
@@ -0,0 +1,69 @@
+from fastapi import APIRouter, HTTPException
+
+from socialapi.models.post import (
+    Comment,
+    CommentIn,
+    UserPost,
+    UserPostIn,
+    UserPostWithComments,
+)
+
+router = APIRouter()
+
+# Temporary in-memory storage
+posts_table = {}
+comments_table = {}
+
+
+def find_post(post_id: int):
+    return posts_table.get(post_id)
+
+
+@router.post("/post", response_model=UserPost, status_code=201)
+async def create_post(post: UserPostIn):
+    data = post.model_dump()  # Use "post.dict()" for Pydantic v1
+    last_record_id = len(posts_table)
+    new_post = {**data, "id": last_record_id}
+    posts_table[last_record_id] = new_post  # Store the new post
+    return new_post
+
+
+@router.get("/post", response_model=list[UserPost])  # List of posts
+async def get_all_posts():
+    return list(posts_table.values())
+
+
+# ---- Comments -----
+@router.post("/comment", response_model=Comment, status_code=201)
+async def create_comment(comment: CommentIn):
+    # Validate that the post exists
+    post = find_post(comment.post_id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+
+    data = comment.model_dump()
+    last_record_id = len(comments_table)
+    new_comment = {**data, "id": last_record_id}
+    comments_table[last_record_id] = new_comment
+    return new_comment
+
+
+@router.get("/post/{post_id}/comments", response_model=list[Comment])
+# pydantic detects the post_id from the path
+async def get_comments_on_post(post_id: int):
+    return [
+        comment for comment in comments_table.values() if comment["post_id"] == post_id
+    ]
+
+
+@router.get("/post/{post_id}", response_model=UserPostWithComments)
+async def get_post_with_comments(post_id: int):
+    post = find_post(post_id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+
+    # The output must match the UserPostWithComments model
+    return {
+        "post": post,
+        "comments": await get_comments_on_post(post_id),
+    }
